b1ea610cf252ad528e3e5a773edc6b4fff22ee48
raimu <ortwang@googlemail.com>
2014-04-06 10:59:23 +0200

    fizzbuzz: refactor

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -2,10 +2,4 @@
 
 
 def fizzbuzz(param):
-    if param % 3 == 0 and param % 5 == 0:
-        return "FizzBuzz"
-    if param % 3 == 0:
-        return "Fizz"
-    if param % 5 == 0:
-        return "Buzz"
-    return param
+    return (not param % 3) * 'Fizz' + (not param % 5) * 'Buzz' or param
