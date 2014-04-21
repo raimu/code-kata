d2562c02f54fce1e140914d6b8079591feeb81f7
raimu <ortwang@googlemail.com>
2014-04-21 22:02:44 +0200

    RomanCalculator: begin with a first test - roman to integer

--- RomanCalculator/roman.py
+++ RomanCalculator/roman.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python
+
+
+class Roman(object):
+
+    arabic_roman_map = [
+        (10, 'X'),
+        (9, 'IX'),
+        (5, 'V'),
+        (4, 'IV'),
+        (1, 'I'),
+    ]
+
+    def __init__(self, number):
+        self.number = number
+
+    def __int__(self):
+        result = 0
+        number = self.number
+        for arabic, roman in self.arabic_roman_map:
+            while number.startswith(roman):
+                result += arabic
+                number = number[len(roman):]
+        return result
--- RomanCalculator/test_roman.py
+++ RomanCalculator/test_roman.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python
+from RomanCalculator.roman import Roman
+
+
+ARABIC_TO_ROMAN = [
+    (1, "I"),
+    (2, "II"),
+    (3, "III"),
+    (4, "IV"),
+    (5, "V"),
+    (8, "VIII"),
+    (9, "IX"),
+    (10, "X"),
+]
+
+
+def test_roman_to_int():
+    for arabic, roman in ARABIC_TO_ROMAN:
+        assert int(Roman(roman)) == arabic
+
