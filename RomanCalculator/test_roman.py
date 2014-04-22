c36ef88219d26c2b8653a677340d8b2c3ec3e335
raimu <ortwang@googlemail.com>
2014-04-22 21:21:12 +0200

    RomanCalculator: roman test up to 3000

--- RomanCalculator/test_roman.py
+++ RomanCalculator/test_roman.py
@@ -11,6 +11,15 @@ ARABIC_TO_ROMAN = [
     (8, "VIII"),
     (9, "IX"),
     (10, "X"),
+    (14, "XIV"),
+    (42, "XLII"),
+    (80, "LXXX"),
+    (94, "XCIV"),
+    (100, "C"),
+    (444, "CDXLIV"),
+    (553, "DLIII"),
+    (911, "CMXI"),
+    (2014, "MMXIV"),
 ]
 
 
