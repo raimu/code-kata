e4a48925d6b3295ec83d553dd581576d93153c87
raimu <ortwang@googlemail.com>
2016-09-14 16:42:41 +0200

    Create empty java project for the fizzbuzz kata
    Just create an empty project. Use gradle to build the files, and junit
    as test framework.

---
+++ java/fizzbuzz/src/main/java/com/github/raimu/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,4 @@
+package com.github.raimu.fizzbuzz;
+
+public class FizzBuzz {
+}
---
+++ java/fizzbuzz/src/test/java/com/github/raimu/fizzbuzz/TestFizzBuzz.java
@@ -0,0 +1,12 @@
+package com.github.raimu.fizzbuzz;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestFizzBuzz {
+    @Test
+    public void testSomething()
+    {
+        assertEquals("", "");
+    }
+}
