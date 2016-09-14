a645fa58faaf09f60df5ed00ae9586540e407652
raimu <ortwang@googlemail.com>
2016-09-14 17:05:09 +0200

    Implement the fizzbuzz kata in java
    The first naiv implementation of the fizzbuzz kata. It looks maybe
    not nice, but functionally it works.

--- java/fizzbuzz/src/main/java/com/github/raimu/fizzbuzz/FizzBuzz.java
+++ java/fizzbuzz/src/main/java/com/github/raimu/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,15 @@
 package com.github.raimu.fizzbuzz;
 
 public class FizzBuzz {
+    public static String getFizzBuzz(int number) {
+        StringBuilder result = new StringBuilder();
+        if (number % 3 == 0) {
+            result.append("Fizz");
+        }
+        if (number % 5 == 0) {
+            result.append("Buzz");
+        }
+
+        return result.length() == 0 ? Integer.toString(number) : result.toString();
+    }
 }
--- java/fizzbuzz/src/test/java/com/github/raimu/fizzbuzz/TestFizzBuzz.java
+++ java/fizzbuzz/src/test/java/com/github/raimu/fizzbuzz/TestFizzBuzz.java
@@ -1,12 +1,30 @@
 package com.github.raimu.fizzbuzz;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class TestFizzBuzz {
+
     @Test
-    public void testSomething()
-    {
-        assertEquals("", "");
+    public void testFizzbuzzShouldReturnNumberAsDefault() {
+        assertEquals("1", FizzBuzz.getFizzBuzz(1));
+        assertEquals("4", FizzBuzz.getFizzBuzz(4));
+    }
+
+    @Test
+    public void testFizzBuzzShouldReturnFizzWhenDivisibleByThree() {
+        assertEquals("Fizz", FizzBuzz.getFizzBuzz(3));
+        assertEquals("Fizz", FizzBuzz.getFizzBuzz(6));
+    }
+
+    @Test
+    public void testFizzBuzzShouldReturnBuzzWhenDivisibleByFive() {
+        assertEquals("Buzz", FizzBuzz.getFizzBuzz(5));
+        assertEquals("Buzz", FizzBuzz.getFizzBuzz(10));
+    }
+
+    @Test
+    public void testFizzBuzzShouldReturnFizzBuzzWhenDivisibleByThreeAndFive() {
+        assertEquals("FizzBuzz", FizzBuzz.getFizzBuzz(15));
     }
 }
